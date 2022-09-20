9cc929dc77a23bd059286c3ee5e01899e0e0b43c
Heechan <ckstmznf@naver.com>
2022-09-20 15:55:00 +0900

    set:firebase setting
    Signed-off-by: Heechan <ckstmznf@naver.com>

--- app/src/main/java/com/heechan/placeofmemory/DesignSystemActivity.kt
+++ app/src/main/java/com/heechan/placeofmemory/DesignSystemActivity.kt
@@ -2,9 +2,9 @@ package com.heechan.placeofmemory
 
 import android.os.Bundle
 import com.heechan.placeofmemory.base.BaseActivity
-import com.heechan.placeofmemory.databinding.ActivityMainBinding
+import com.heechan.placeofmemory.databinding.ActivityDesignSystemBinding
 
-class DesignSystemActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_design_system) {
+class DesignSystemActivity : BaseActivity<ActivityDesignSystemBinding>(R.layout.activity_design_system) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
