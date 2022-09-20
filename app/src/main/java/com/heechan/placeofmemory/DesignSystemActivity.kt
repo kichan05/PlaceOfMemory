bfc2335ef738a8d66243e2a162d6b1a61cc2234b
Heechan <ckstmznf@naver.com>
2022-09-20 15:42:40 +0900

    add:icon set
    Signed-off-by: Heechan <ckstmznf@naver.com>

--- app/src/main/java/com/heechan/placeofmemory/DesignSystemActivity.kt
+++ app/src/main/java/com/heechan/placeofmemory/DesignSystemActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import com.heechan.placeofmemory.base.BaseActivity
 import com.heechan.placeofmemory.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+class DesignSystemActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_design_system) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
